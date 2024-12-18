8a4cb4b05536d5801895d5e5a55fc58da81c4672
filk <filkrysiak@gmail.com>
2024-12-18 01:38:14 +0100

    info o planetach

--- my-app/src/components/Planet.jsx
+++ my-app/src/components/Planet.jsx
@@ -1,17 +1,50 @@
 import React, { useRef, useEffect } from 'react';
 import { useLoader, useFrame } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from 'three';
 
-const Planet = ({ modelPath, position, visible, isRotating }) => {
+const Planet = ({ modelPath, position, visible, isOpen }) => {
   const planetRef = useRef();
   const model = useLoader(GLTFLoader, modelPath);
+  const mixerRef = useRef(null);
+  const actionsRef = useRef([]);
 
- 
-  useFrame(() => {
-    if (isRotating && planetRef.current && visible) {
-      
-      planetRef.current.rotation.y += 0.001;
-      //planetRef.current.rotation.x += 0.000005;
+  useEffect(() => {
+    if (model.animations.length) {
+      const mixer = new THREE.AnimationMixer(model.scene);
+      mixerRef.current = mixer;
+
+      const actions = model.animations.map((clip) => {
+        const action = mixer.clipAction(clip);
+        action.loop = THREE.LoopOnce;
+        action.clampWhenFinished = true;
+        action.play();
+        return action;
+      });
+
+      actionsRef.current = actions;
+    }
+  }, [model]);
+
+  useEffect(() => {
+    actionsRef.current.forEach((action) => {
+      if (isOpen) {
+        action.reset();
+        action.time = 0;
+        action.timeScale = 1; 
+        action.play();
+      } else {
+        action.reset();
+        action.time = action.getClip().duration;
+        action.timeScale = -1; 
+        action.play();
+      }
+    });
+  }, [isOpen]);
+
+  useFrame((state, delta) => {
+    if (mixerRef.current) {
+      mixerRef.current.update(delta);
     }
   });
 
---
+++ my-app/src/components/planetData.js
@@ -0,0 +1,89 @@
+export const planets = [
+    { 
+      name: 'Ziemia', 
+      modelPath: '/models/ziema_animacja.glb', 
+      description: 'Trzecia planeta od Słońca, znana jako Błękitna Planeta.',
+      mass:'5.97 × 10²⁴ kg',
+      diameter:'12,742 km',
+      hfar:'149.6 mln km (1 AU)',
+      atmospheree:'Azot (78%), tlen (21%), inne',
+      temp:'Średnio 15°C',
+      dlength:'24 godziny',
+      ylength:'365 dni',
+      moons:'1 (Księżyc)',
+      geology:'Oceany (70%), góry, równiny, aktywne wulkany'
+    },
+    { 
+      name: 'Wenus', 
+      modelPath: '/models/innerCore_animation_close.glb', 
+      description: 'Druga planeta od Słońca, znana z wysokiej temperatury i gęstej atmosfery.',
+      mass:'4.87 × 10²⁴ kg (0.815 Ziemi)',
+      diameter:'12,104 km',
+      hfar:'108.2 mln km (0.72 AU)',
+      atmospheree:'Gęsta: dwutlenek węgla, azot',
+      temp:'Średnio 464°C',
+      dlength:'243 dni (wstecz)',
+      ylength:'225 dni',
+      moons:'Brak',
+      geology:'Wulkany, równiny, gruba warstwa chmur kwasu siarkowego'
+    },
+    
+    /*
+  { 
+      name: 'Merkury', 
+      modelPath: '/models/ziema_animacja.glb', 
+      description: 'Pierwsza i najmniejsza planeta układu słonecznego, cechuje się największymi wachaniami temperatury w Układzie Słonecznym.' ,
+      mass:'3.30 × 10²³ kg (0.055 Ziemi)',
+      diameter:'4,880 km',
+      hfar:'57.91 mln km (0.39 AU)',
+      atmospheree:'',
+      temp:'',
+      dlength:'',
+      ylength:'',
+      moons:'',
+      geology:''
+    },
+    { 
+      name: 'Ziemia', 
+      modelPath: '/models/ziema_animacja.glb', 
+      description: 'Pierwsza i najmniejsza planeta układu słonecznego, cechuje się największymi wachaniami temperatury w Układzie Słonecznym.' ,
+      mass:'5.97 × 10²⁴ kg',
+      diameter:'12,742 km',
+      hfar:'149.6 mln km (1 AU)',
+      atmospheree:'Azot (78%), tlen (21%), inne',
+      temp:'Średnio 15°C',
+      dlength:'24 godziny',
+      ylength:'365 dni',
+      moons:'1 (Księżyc)',
+      geology:'Oceany (70%), góry, równiny, aktywne wulkany'
+    },
+    { 
+      name: 'Wenus', 
+      modelPath: '/models/ziema_animacja.glb', 
+      description: 'Pierwsza i najmniejsza planeta układu słonecznego, cechuje się największymi wachaniami temperatury w Układzie Słonecznym.' ,
+      mass:'4.87 × 10²⁴ kg (0.815 Ziemi)',
+      diameter:'12,104 km',
+      hfar:'108.2 mln km (0.72 AU)',
+      atmospheree:'Gęsta: dwutlenek węgla, azot',
+      temp:'Średnio 464°C',
+      dlength:'243 dni (wstecz)',
+      ylength:'225 dni',
+      moons:'Brak',
+      geology:'Wulkany, równiny, gruba warstwa chmur kwasu siarkowego'
+    },
+    { 
+      name: 'Ziemia', 
+      modelPath: '/models/ziema_animacja.glb', 
+      description: 'Pierwsza i najmniejsza planeta układu słonecznego, cechuje się największymi wachaniami temperatury w Układzie Słonecznym.' ,
+      mass:'',
+      diameter:'',
+      hfar:'',
+      atmospheree:'',
+      temp:'',
+      dlength:'',
+      ylength:'',
+      moons:'',
+      geology:''
+    },
+     */
+  ];
